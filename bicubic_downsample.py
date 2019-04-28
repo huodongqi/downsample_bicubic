c46873776751790cfe72e0d00d94da3cc56ab66b
HDQ <38202286+huodongqi@users.noreply.github.com>
2019-04-28 11:10:51 +0800

    Update bicubic_downsample.py

--- bicubic_downsample.py
+++ bicubic_downsample.py
@@ -1,13 +1,13 @@
 import cv2
 import os
-if 'data' not in os.listdir('./OpenCV/'):
-    os.mkdir('./OpenCV/data') 
-path = './Self-Attention-GAN/data/CelebA/CelebA/'
+if 'data' not in os.listdir('./result'):
+    os.mkdir('./result') 
+path = './data'
 files = os.listdir(path)
 for file in files:
     img = cv2.imread(path + '/' + file)
     x = img.shape[1]/4
     y = img.shape[0]/4
     img = cv2.resize(img, (int(x), int(y)),interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('./OpenCV/data/' + file[:-4] + '.png', img)
+    cv2.imwrite('./data/' + file[:-4] + '.png', img)
     print(file[:-4] + '.png' + ' have been transformed!')
